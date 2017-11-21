cc8396a17b8fa68fe70ff3eb85f54657df0bea95
jihuaixi <2579830610@qq.com>
2017-11-21 13:11:42 +0800

    修改家族史文本框问题

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -23,6 +23,9 @@ import javax.swing.DefaultComboBoxModel;
 import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JCheckBox;
+import java.awt.Color;
+import javax.swing.JRadioButton;
 
 public class Menu extends JFrame {
 
@@ -77,12 +80,22 @@ public class Menu extends JFrame {
 		
 		JMenu menu_1 = new JMenu("帮助");
 		menuBar.add(menu_1);
+		
+		JMenuItem menuItem_1 = new JMenuItem("填写帮助");
+		menu_1.add(menuItem_1);
+		
+		JMenuItem menuItem_2 = new JMenuItem("软件使用");
+		menu_1.add(menuItem_2);
+		
+		JMenuItem menuItem_3 = new JMenuItem("联系开发者");
+		menu_1.add(menuItem_3);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel Info = new JPanel();
+		Info.setBackground(Color.LIGHT_GRAY);
 		Info.setToolTipText("Info");
 		Info.setBounds(12, 12, 1142, 53);
 		contentPane.add(Info);
@@ -129,10 +142,47 @@ public class Menu extends JFrame {
 		JPanel GravidityState = new JPanel();
 		GravidityState.setBounds(12, 77, 1142, 196);
 		contentPane.add(GravidityState);
+		GravidityState.setLayout(null);
+		
+		JLabel label_9 = new JLabel("怀孕状况");
+		label_9.setBounds(0, 87, 63, 13);
+		GravidityState.add(label_9);
 		
 		JPanel MarriageState = new JPanel();
+		MarriageState.setBackground(Color.LIGHT_GRAY);
 		MarriageState.setBounds(12, 281, 612, 74);
 		contentPane.add(MarriageState);
+		MarriageState.setLayout(null);
+		
+		JLabel label_2 = new JLabel("婚育状况");
+		label_2.setBounds(0, 31, 52, 13);
+		MarriageState.add(label_2);
+		
+		JLabel label_4 = new JLabel("次数：");
+		label_4.setBounds(294, 12, 52, 13);
+		MarriageState.add(label_4);
+		
+		JCheckBox chckbxNewCheckBox = new JCheckBox("有子女");
+		chckbxNewCheckBox.setBounds(378, 45, 73, 21);
+		MarriageState.add(chckbxNewCheckBox);
+		
+		JCheckBox checkBox = new JCheckBox("配偶再婚");
+		checkBox.setBounds(260, 45, 86, 21);
+		MarriageState.add(checkBox);
+		
+		JCheckBox checkBox_1 = new JCheckBox("目前再婚");
+		checkBox_1.setBounds(82, 45, 86, 21);
+		MarriageState.add(checkBox_1);
+		
+		JCheckBox checkBox_2 = new JCheckBox("有过主动流产");
+		checkBox_2.setBounds(82, 8, 116, 21);
+		MarriageState.add(checkBox_2);
+		
+		JComboBox comboBox_2 = new JComboBox();
+		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"}));
+		comboBox_2.setSelectedIndex(0);
+		comboBox_2.setBounds(375, 7, 63, 22);
+		MarriageState.add(comboBox_2);
 		
 		JPanel MensesState = new JPanel();
 		MensesState.setBounds(12, 363, 612, 155);
@@ -141,8 +191,21 @@ public class Menu extends JFrame {
 		JPanel AllergyHistory = new JPanel();
 		AllergyHistory.setBounds(12, 527, 612, 88);
 		contentPane.add(AllergyHistory);
+		AllergyHistory.setLayout(null);
+		
+		JLabel label_6 = new JLabel("过敏史");
+		label_6.setBounds(0, 33, 39, 13);
+		AllergyHistory.add(label_6);
+		
+		JScrollPane scrollPane_2 = new JScrollPane();
+		scrollPane_2.setBounds(50, 0, 562, 88);
+		AllergyHistory.add(scrollPane_2);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane_2.setViewportView(textArea);
 		
 		JPanel FamilyState = new JPanel();
+		FamilyState.setBackground(Color.LIGHT_GRAY);
 		FamilyState.setBounds(12, 627, 612, 81);
 		contentPane.add(FamilyState);
 		FamilyState.setLayout(null);
@@ -151,6 +214,7 @@ public class Menu extends JFrame {
 		scrollPane.setBounds(48, 0, 564, 81);
 		FamilyState.add(scrollPane);
 		
+		
 		JTextArea FamilyStateTextArea = new JTextArea();
 		FamilyStateTextArea.setText("例如：家族性遗传病，父母有无高血压病，糖尿病，高血脂，脑梗等;有无近亲结婚史");
 		FamilyStateTextArea.addFocusListener(new FocusAdapter() {
@@ -158,7 +222,17 @@ public class Menu extends JFrame {
 			@Override
 			public void focusGained(FocusEvent arg0) {
 				// TODO Auto-generated method stub
-				FamilyStateTextArea.setText("");
+				if(FamilyStateTextArea.getText().equals("例如：家族性遗传病，父母有无高血压病，糖尿病，高血脂，脑梗等;有无近亲结婚史")) {
+					FamilyStateTextArea.setText("");
+				}				
+			}
+
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				if(FamilyStateTextArea.getText().length()<1) {
+					FamilyStateTextArea.setText("例如：家族性遗传病，父母有无高血压病，糖尿病，高血脂，脑梗等;有无近亲结婚史");
+				}
 			}
 			
 			
@@ -177,15 +251,101 @@ public class Menu extends JFrame {
 
 			}
 		});
-		saveButton.setBounds(547, 720, 104, 23);
+		saveButton.setBounds(576, 706, 104, 23);
 		contentPane.add(saveButton);
 		
 		JPanel PastHistory = new JPanel();
 		PastHistory.setBounds(636, 281, 518, 237);
 		contentPane.add(PastHistory);
+		PastHistory.setLayout(null);
+		
+		JLabel label_3 = new JLabel("既往史");
+		label_3.setBounds(12, 112, 39, 13);
+		PastHistory.add(label_3);
+		
+		JLabel label_7 = new JLabel("高血压");
+		label_7.setBounds(164, 12, 63, 13);
+		PastHistory.add(label_7);
+		
+		JRadioButton radioButton = new JRadioButton("有");
+		radioButton.setBounds(241, 8, 47, 21);
+		PastHistory.add(radioButton);
+		
+		JRadioButton radioButton_1 = new JRadioButton("没有");
+		radioButton_1.setBounds(287, 4, 69, 21);
+		PastHistory.add(radioButton_1);
+		
+		JLabel label_8 = new JLabel("糖尿病");
+		label_8.setBounds(164, 37, 63, 13);
+		PastHistory.add(label_8);
+		
+		JRadioButton radioButton_2 = new JRadioButton("有");
+		radioButton_2.setBounds(241, 33, 47, 21);
+		PastHistory.add(radioButton_2);
+		
+		JRadioButton radioButton_3 = new JRadioButton("没有");
+		radioButton_3.setBounds(287, 29, 69, 21);
+		PastHistory.add(radioButton_3);
+		
+		JRadioButton radioButton_4 = new JRadioButton("有");
+		radioButton_4.setBounds(241, 58, 47, 21);
+		PastHistory.add(radioButton_4);
+		
+		JRadioButton radioButton_5 = new JRadioButton("没有");
+		radioButton_5.setBounds(287, 54, 69, 21);
+		PastHistory.add(radioButton_5);
+		
+		JRadioButton radioButton_6 = new JRadioButton("有");
+		radioButton_6.setBounds(241, 83, 47, 21);
+		PastHistory.add(radioButton_6);
+		
+		JRadioButton radioButton_7 = new JRadioButton("没有");
+		radioButton_7.setBounds(287, 79, 69, 21);
+		PastHistory.add(radioButton_7);
+		
+		JRadioButton radioButton_8 = new JRadioButton("有");
+		radioButton_8.setBounds(241, 108, 47, 21);
+		PastHistory.add(radioButton_8);
+		
+		JRadioButton radioButton_9 = new JRadioButton("没有");
+		radioButton_9.setBounds(287, 104, 69, 21);
+		PastHistory.add(radioButton_9);
 		
 		JPanel OperationHistory = new JPanel();
 		OperationHistory.setBounds(636, 527, 518, 181);
 		contentPane.add(OperationHistory);
+		OperationHistory.setLayout(null);
+		
+		JLabel label_5 = new JLabel("手术史");
+		label_5.setBounds(12, 76, 63, 13);
+		OperationHistory.add(label_5);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(82, 0, 436, 181);
+		OperationHistory.add(scrollPane_1);
+		
+		JTextArea OperationHistoryTextArea = new JTextArea();		
+		scrollPane_1.setViewportView(OperationHistoryTextArea);
+		OperationHistoryTextArea.addFocusListener(new FocusAdapter() {
+
+			@Override
+			public void focusGained(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				if(OperationHistoryTextArea.getText().equals("例如：宫腔镜，腹腔镜，开腹手术，输卵")) {
+					OperationHistoryTextArea.setText("");
+				}				
+			}
+
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				// TODO Auto-generated method stub
+				if(OperationHistoryTextArea.getText().length()<1) {
+					OperationHistoryTextArea.setText("例如：宫腔镜，腹腔镜，开腹手术，输卵");
+				}
+			}
+			
+			
+		});
+		
 	}
 }
